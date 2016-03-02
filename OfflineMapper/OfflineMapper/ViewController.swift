dfc7487563d88cad1d45a2f5a2dab36d55cac789
Sergii Nezdolii <sergii.nezdolii@gmail.com>
2016-03-02 11:19:11 +0100

    Initial project commit

---
+++ OfflineMapper/OfflineMapper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OfflineMapper
+//
+//  Created by Sergii Nezdolii on 02/03/16.
+//  Copyright © 2016 FrostDigital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
