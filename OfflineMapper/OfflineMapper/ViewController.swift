dccf3ac14e72f05027ca4658e08bea3156adbe86
Sergii Nezdolii <sergii.nezdolii@gmail.com>
2016-03-02 18:15:29 +0100

    Adding Map type switcher - not cached yet

--- OfflineMapper/OfflineMapper/ViewController.swift
+++ OfflineMapper/OfflineMapper/ViewController.swift
@@ -9,26 +9,62 @@
 import UIKit
 import MapKit
 
+enum MapTileTemplate: String {
+    case OpenStreetMap = "http://tile.openstreetmap.org/{z}/{x}/{y}.png",
+    OpenCycleMap = "http://b.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png",
+    MapQuest = "http://otile3.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.jpg"
+}
+
 class ViewController: UIViewController, MKMapViewDelegate {
 
     //MARK: Constants
-    let osmTemplate = "http://tile.openstreetmap.org/{z}/{x}/{y}.png"
+    let kOpenStreetMapTemplate  = "http://tile.openstreetmap.org/{z}/{x}/{y}.png"
+    let kOpenCycleMapTemplate   = "http://b.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png"
+    let kMapQuestTemplate       = "http://otile3.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.jpg"
+    let kLantmaterietTemplate   = "http://maps-open.lantmateriet.se/open/topowebb-ccby/v1/wmts/1.0.0/topowebb/default/3006/{z}/{x}/{y}.png"
+    let kOpenSeaMapTemplate     = "http://tiles.openseamap.org/seamark/{z}/{x}/{y}.png"
     
     //MARK: IBOutlets
 
+    @IBOutlet weak var mapTypeSegmentedControl: UISegmentedControl!
     @IBOutlet weak var mapView: MKMapView!
     
     //MARK: ViewController funcs
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        initMap()
+        mapTypeChanged(mapTypeSegmentedControl)
+    }
+    
+    @IBAction func mapTypeChanged(sender: AnyObject?) {
+        addTileOverlayWithTemplate(mapTileTemplate(mapTypeSegmentedControl.selectedSegmentIndex))
     }
     
     //MARK: Helper funcs
     
+    private func mapTileTemplate(selectedIndex: Int) -> MapTileTemplate {
+        switch selectedIndex {
+        case 0: return .OpenStreetMap
+        case 1: return .OpenCycleMap
+        case 2: return .MapQuest
+        default: return .OpenStreetMap
+        }
+    }
+    
+    private func addTileOverlayWithTemplate(mapTileTemplate: MapTileTemplate) {
+        //Remove old tile overlays, as they are not needed
+        let tileOverlays = mapView.overlays.filter { $0 is MKTileOverlay }
+        if tileOverlays.count > 0 {
+            mapView.removeOverlays(tileOverlays)
+        }
+        
+        let tileOverlay = MKTileOverlay(URLTemplate: mapTileTemplate.rawValue)
+        tileOverlay.canReplaceMapContent = true
+        mapView.addOverlay(tileOverlay)
+    }
+    
     private func initMap() {
-        let osmOverlay = MKTileOverlay(URLTemplate: osmTemplate)
+        let osmOverlay = MKTileOverlay(URLTemplate: kOpenStreetMapTemplate)
         osmOverlay.canReplaceMapContent = true
         
         mapView.addOverlay(osmOverlay)
